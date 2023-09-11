8c87f232b593bdbe477e779fd544e14ed1018a2d
Dom <dominic.vega@avegabros.com>
2023-09-11 09:41:24 +0800

    update

--- components/ProtectedRoute.jsx
+++ components/ProtectedRoute.jsx
@@ -1,12 +1,15 @@
 // components/ProtectedRoute.js
 import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import { isAuthenticated } from 'services/authService';
 
 const ProtectedRoute = ({ children }) => {
+  const router = useRouter();
+
   useEffect(() => {
     if (!isAuthenticated()) {
       // Redirect to the login page or show an error message
-      window.location.href = '/auth/login';
+      router.push('/auth/login');
     }
   }, []);
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,12 +3,13 @@ import Image from 'next/image'
 import ProtectedRoute from 'components/ProtectedRoute';
 import Link from 'next/link';
 import { logout } from 'services/authService';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Navbar from '../../components/Navbar';
 
 export default function Home() {
   const router = useRouter();
+  const [isMounted, setIsMounted] = useState(false);
 
   const handlelogout = async (e) => {
     e.preventDefault();
@@ -16,18 +17,21 @@ export default function Home() {
       router.push('/login');
     }
   }
+  useEffect(() => {
+    setIsMounted(true); // Set the component as mounted when it's mounted
+  }, []);
   
   return (
-    <ProtectedRoute>
-      <Navbar />
-      <main>
-        <div>
+    <div>
+      {isMounted && (
+        <ProtectedRoute>
           <div>
+            <Navbar/>
             <h1>Dashboard Page (Protected)</h1>
             <p>This page is protected and can only be accessed by authenticated users.</p>
           </div>
-        </div>
-      </main>
-    </ProtectedRoute>
+        </ProtectedRoute>
+      )}
+    </div>
   )
 }
