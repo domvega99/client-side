470b840cefe3d44c1dbf353de72af128c252c19c
Dom <dominic.vega@avegabros.com>
2023-09-11 09:08:54 +0800

    update

--- services/authService.js
+++ services/authService.js
@@ -1,35 +1,30 @@
-// services/authService.js
-import axios from 'axios';
 import jwt from 'jsonwebtoken';
 
-// export const login = async (username, password) => {
-  
-
-// };
 
 export const logout = () => {
-  if (typeof window !== "undefined") {
-  localStorage.removeItem('token');
-  localStorage.removeItem('user');
-  window.location.href = '/auth/login';
+  if (typeof localStorage !== 'undefined') {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
   }
+  if (typeof window !== 'undefined') {
+    window.location.href = '/auth/login';
+  }
+
 };
 
 export const isAuthenticated = () => {
   // Check if the user is authenticated by verifying the token
+  if (typeof localStorage !== 'undefined') {
+    const token = localStorage.getItem('token');
 
-    if (localStorage != undefined) {
-      const token = localStorage.getItem('token');
+    if (token) {
+      const decodedToken = jwt.decode(token);
 
-        if (token) {
-          const decodedToken = jwt.decode(token);
-
-          if (decodedToken) {
-            return true;
-          }
-        }
-
-        return false;
+      if (decodedToken) {
+        return true;
       }
-    
+    }
+  }
+
+  return false;
 };
